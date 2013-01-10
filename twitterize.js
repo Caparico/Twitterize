5cbe06e1946c1b894d3c0d26e6d848d62da7f485
Caparico <kobi.birdman@gmail.com>
2013-01-10 22:37:09 +0200

    delete local jquery copy. reference Google CDN. Advance plugin code.

--- twitterize.js
+++ twitterize.js
@@ -6,7 +6,7 @@ if (typeof Object.create !== 'function') {	//if the browser doesn't support 'Obj
 		return new F();	// launch a new instatnce of this function, after it had inheritted methods from the 'Twitter' object I define. This instance is then stored in the 'twitter' object.
 	};
 }
-(function($, window, document, undefined) {	// passing a slef executing function to make sure my plugin doesn't clash with any other script that might be using the $ to refer to something else. Also, adding common parameters like window & document.
+(function($, window, document, undefined) {	// passing a slef executing function to make sure my plugin doesn't clash with any other script that might be using the $ to refer to something else. Also, adding common parameters like window & document & undefined.
 	var Twitter = {
 		init: function(options, elem) {	// an 'init' method, to  get the plugin started.
 			var self = this;
@@ -16,13 +16,42 @@ if (typeof Object.create !== 'function') {	//if the browser doesn't support 'Obj
 
 			self.url = 'http://search.twitter.com/search.json';
 
-			if (typeof options == 'string') { //if the user had passed a string to search for.
+			if (typeof options === 'string') { //if the user had passed a string to search for.
 				self.search = options;
 			} else { // the user had passed an object.
 				self.search = options.search;
 				self.options = $.extend( {}, $.fn.queryTwitter.options, options );
-				console.log(self.options);
-			};
+			}
+
+			self.cycle();	// this will be the method that calls everything and displays it. 
+		},
+		cycle: function() {
+			var self = this;
+
+			self.fetch().done(function(results) {
+				self.buildFragment(results);
+				self.display();
+			});
+		},
+
+		fetch: function() {	// this will be the method that retrieves the relevant tweets from Twitter.
+			return $.ajax({
+				url: this.url,
+				data: {q: this.search},
+				datatype: 'jsonp'
+			});
+		},
+
+		buildFragment: function(results) {
+			var self = this;
+
+			self.tweets = $.map(results.results, function(obj, i) {
+				console.log(obj);
+			});
+		},
+
+		display: function() {	// this will be the method that displays the tweets that were retrieved onto the screen. 
+			this.$elem.html(self.tweets);	//is that available?? I am referencing the search term which I cached earlier into $elem.
 		}
 	};
 	$.fn.queryTwitter = function(options) {
@@ -33,7 +62,7 @@ if (typeof Object.create !== 'function') {	//if the browser doesn't support 'Obj
 	};
 
 	$.fn.queryTwitter.options = { // Defining the OPTIONS for the plugin. The user may access the 'options' object, and change them as per his/her defaults.
-		search: '#preendotme'
+		search: 'preendotme'
 	};
 
-})(jQuery, window, document);	// passing the parameters from the top, so it creates a "local scope" for them in order to acheive a small performance improvement when the code is minified. I am also not passing anything to the 'undefined' parameter, so that it always stays equal to 'undefined' (undefined=undefined).
+})(jQuery, window, document);	// passing the parameters from the top, so it creates a "local scope" for them, as a precaution. Also, not passing anything to the 'undefined' parameter, so that it always stays equal to 'undefined' (undefined=undefined).
